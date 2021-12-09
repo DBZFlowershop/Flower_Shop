ebc1dd3433c427793132a80d8fb0d011d212cd0c
iamjinseo <58293743+iamjinseo@users.noreply.github.com>
2021-12-09 13:29:35 +0900

    꽃 데이터 관리하는 php파일 업로드

---
+++ flower.php
@@ -0,0 +1,179 @@
+<? php
+/* dbconfig */
+$servername = "localhost:3306";
+$username = "root";
+$password = "root";
+
+$conn = new mysqli($servername, $username, $password);
+/* include_once */
+
+/* db 생성 */
+$create_db = "CREATE DATABASE flower_shop";
+
+/* db 생성 후 쿼리 전송 */
+if($conn->query($create_db) === TRUE){
+
+} else {
+
+}
+
+/* 꽃 테이블 생성 */
+$dbname = "flower_shop";
+mysqli_select_db($conn, $dbname) or die("DB selection failed");
+
+$create_flower_table = "CREATE TABLE Flower(
+    FlowerID int unsigned,
+    FlowerName varchar(30) not null,
+    FlowerPrice int unsigned not null,
+    Category varchar(10) not null,
+    FlowerImg varchar(30) not null,
+
+    primary key(FlowerID)
+)";
+
+/* 상황별 꽃들 삽입 */
+$insert_situation_flowers = "INSERT INTO Flower VALUES
+    (001, 'Camomile', 25, 'situation', 'contex1.jpg'),
+    (002, 'Papiopedilloom', 35, 'situation', 'contex2.jpg')
+    (003, 'Gongjakcho', 20, 'situation', 'contex3.jpg'),
+    (004, 'freesia', 25, 'situation', 'contex4.jpg'),
+    (005, 'Carnation', 30, 'situation', 'contex5.jpg'),
+    (006, 'Pink hydrange', 20, 'situation', 'contex6.jpeg'),
+    (007, 'Cotton', 25, 'situation', 'contex7.jpg'),
+    (008, 'buque', 20, 'situation', 'contex8.jpg')";
+
+/* 상황별 꽃 설명 테이블 weak entity 생성*/
+$create_explanation = "CREATE TABLE FlowerExplanation( 
+    FlowerID INT UNSIGNED REFERENCES Flowers(FlowerID) ON DELETE CASCADE, /* Flowers에 종속됨 */
+    Explanation VARCHAR(80) NOT NULL,
+    PRIMARY KEY(FlowerID)
+)";
+
+/* 상황별 꽃에 설명 삽입 */
+$insert_explanation = "INSERT INTO FlowerExplanation(
+    (001, 'When visiting a person in a difficult situation or'),
+    (002, 'When encouraging the future of good people')
+    (003, 'When you want to make peace'),
+    (004, 'When cheering for a new beginning'),
+    (005, 'Mother's Day'),
+    (006, 'Couple's anniversary'),
+    (007, 'Christmas'),
+    (008, 'Wedding')
+)";
+
+/* 계절별 꽃 삽입 */
+$insert_seasonal_flowers = "INSERT INTO Flower VALUES
+    (009, 'Pink Lanumculus', 25, 'Spring', 'spring1.png'),
+    (010, 'Georber', 35, 'Spring', 'spring2.png'),
+    (011, 'Gongjakcho', 20, 'Spring', 'spring3.jpg'),
+    (012, 'Yellow prisia', 25, 'Spring', 'spring4.jpg'),
+    (013, 'Pink Tulip', 30, 'Spring', 'spring5.jpeg'),
+    (014, 'Oxford, Butterfly', 20, 'Spring', 'spring6.jpeg'),
+    (015, 'Big flower', 70, 'Spring', 'spring7.jpeg'),
+    (016, 'Mango tulip', 20, 'Spring', 'spring8.jpg'),
+    (017, 'Purple hydrangea', 25, 'Summer', 'summer1.jpg'),
+    (018, 'Yellow rose', 35, 'Summer', 'summer2.jpeg'),
+    (019, 'Blue rose', 20, 'Summer', 'summer3.jpeg'),
+    (020, 'Lavenderace rose', 25, 'Summer', 'summer4.jpeg'),
+    (021, 'delphinium', 30, 'Summer', 'summer5.jpeg',
+    (022, 'Blue hydrangea', 20, 'Summer', 'summer6.jpeg'),
+    (023, 'Calla', 20, 'Summer', 'summer7.jpeg'),
+    (024, 'Pink hydrange', 25, 'Summer', 'summer8.jpeg'),
+    (025, 'Yellow, Pink rose', 20, 'Summer', 'summer9.jpeg'),
+    (026, 'Mixed flower', 25, 'Summer', 'summer10.jpeg'),
+    (027, 'Mini rose', 20, 'Summer', 'summer11.jpeg'),
+    (028, 'Green hydrange', 30, 'Summer', 'summer12.jpeg'),
+    (029, 'sunflower', 30, 'Fall', 'fall1.jpg'),
+    (030, 'Chrysanthemum', 30, 'Fall', 'fall2.jpg'),
+    (031, 'White Chrysanthemum', 20, 'Fall', 'fall3.jpg'),
+    (032, 'Purple globe amaranth', 25, 'Fall', 'fall4.jpg'),
+    (033, 'Red globe amaranth', 30, 'Fall', 'fall5.jpg'),
+    (034, 'Pink Patrinia scabiosaefolia', 20, 'Fall', 'fall6.jpg'),
+    (035, 'Purple Patrinia scabiosaefolia', 20, 'Fall', 'fall7.jpg'),
+    (036, 'Yellow Patrinia scabiosaefolia', 25, 'Fall', 'fall8.jpg'),
+    (037, 'Pink dahlia', 20, 'Fall', 'fall9.jpg'),
+    (038, 'burgundy dahlia', 25, 'Fall', 'fall10.jpg'),
+    (039, 'Orange dahlia', 20, 'Fall', 'fall11.jpeg'),
+    (029, 'Cosmos', 30, 'Fall', 'fall12.jpg'),
+    (030, 'Cotton flower', 25, 'Winter', 'winter1.jpg'),
+    (031, 'Stoke', 30, 'Winter', 'winter2.jpg'),
+    (032, 'Siklamen', 30, 'Winter', 'winter3.jpg')
+";
+
+/* flower 테이블에 커스텀 꽃 삽입 */
+$insert_custom_flowers = "INSERT INTO Flower(FlowerID, FlowerName, FlowerPrice, Categroy, FlowerImg) VALUES 
+    (049, 'Custom1', 30, 'Custom', 'pros.png'),
+    (050, 'Custom2', 50, 'Custom', 'prom.png'),
+    (051, 'Custom3', 70, 'Custom', 'prol.png'),
+    (052, 'Custom4', 30, 'Custom', 'pts.png'),
+    (053, 'Custom5', 50, 'Custom', 'ptm.png'),
+    (054, 'Custom6', 70, 'Custom', 'ptl.png'),
+    (055, 'Custom7', 30, 'Custom', 'pras.png'),
+    (056, 'Custom8', 50, 'Custom', 'pram.png'),
+    (057, 'Custom9', 70, 'Custom', 'pral.png'),
+
+    (058, 'Custom10', 30, 'Custom', 'wros.png'),
+    (059, 'Custom11', 50, 'Custom', 'wrom.png'),
+    (060, 'Custom12', 70, 'Custom', 'wrol.png'),
+    (061, 'Custom13', 30, 'Custom', 'wts.png'),
+    (062, 'Custom14', 50, 'Custom', 'wtm.png'),
+    (063, 'Custom15', 70, 'Custom', 'wtl.png'),
+    (064, 'Custom16', 30, 'Custom', 'wras.png'),
+    (065, 'Custom17', 50, 'Custom', 'wram.png'),
+    (066, 'Custom18', 70, 'Custom', 'wral.png'),
+
+    (067, 'Custom19', 30, 'Custom', 'gros.png'),
+    (068, 'Custom20', 50, 'Custom', 'grom.png'),
+    (069, 'Custom21', 70, 'Custom', 'grol.png'),
+    (070, 'Custom22', 30, 'Custom', 'gts.png'),
+    (071, 'Custom23', 50, 'Custom', 'gtm.png'),
+    (072, 'Custom24', 70, 'Custom', 'gtl.png'),
+    (073, 'Custom25', 30, 'Custom', 'gras.png'),
+    (074, 'Custom26', 50, 'Custom', 'gram.png'),
+    (075, 'Custom27', 70, 'Custom', 'gral.png')
+";
+
+/* 커스텀 꽃 테이블 생성 */
+$create_custom_table = "CREATE TABLE Custom(
+    CustomID INT,
+    FlowerID INT, 
+    WrapColor VARCHAR(10) NOT NULL,
+    FlowerType VARCHAR(20) NOT NULL,
+    Size VARCHAR(10) NOT NULL,
+
+    PRIMARY KEY(CustomID)
+)";
+
+/* Custom 테이블에 커스텀 꽃 삽입 */
+$insert_custom_custom = "INSERT INTO Custom(CustomID, FlowerID, WrapColor, FlowerType, Size) VALUES 
+    (1, 049, 'Pink', 'Rose', 'Small'),
+    (2, 050, 'Pink', 'Rose', 'Medium'),
+    (3, 051, 'Pink', 'Rose', 'Large'),
+    (4, 052, 'Pink', 'Tulip', 'Small'),
+    (5, 053, 'Pink', 'Tulip', 'Medium'),
+    (6, 054, 'Pink', 'Tulip', 'Large'),
+    (7, 055, 'Pink', 'Ranunculus', 'Small'),
+    (8, 056, 'Pink', 'Ranunculus', 'Medium'),
+    (9, 057, 'Pink', 'Ranunculus', 'Large'),
+
+    (10, 058, 'White', 'Rose', 'Small'),
+    (11, 059, 'White', 'Rose', 'Medium'),
+    (12, 060, 'White', 'Rose', 'Large'),
+    (13, 061, 'White', 'Tulip', 'Small'),
+    (14, 062, 'White', 'Tulip', 'Medium'),
+    (15, 063, 'White', 'Tulip', 'Large'),
+    (16, 064, 'White', 'Ranunculus', 'Small'),
+    (17, 065, 'White', 'Ranunculus', 'Medium'),
+    (18, 066, 'White', 'Ranunculus', 'Large'),
+
+    (19, 067, 'Gray', 'Rose', 'Small'),
+    (20, 068, 'Gray', 'Rose', 'Medium'),
+    (21, 069, 'Gray', 'Rose', 'Large'),
+    (22, 070, 'Gray', 'Tulip', 'Small'),
+    (23, 071, 'Gray', 'Tulip', 'Medium'),
+    (24, 072, 'Gray', 'Tulip', 'Large'),
+    (25, 073, 'Gray', 'Ranunculus', 'Small'),
+    (26, 074, 'Gray', 'Ranunculus', 'Medium'),
+    (27, 075, 'Gray', 'Ranunculus', 'Large')
+";
+?>
